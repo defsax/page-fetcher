da0ec4e43e3fc4edbcf28d1b25b6237926f88005
Perry Defayette <defayetteperry@gmail.com>
2021-01-14 01:30:38 +0000

    Completed with 1 edge case handled.

--- fetcher.js
+++ fetcher.js
@@ -1,29 +1,69 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
+//check how many arguments are passed in.
+//should only be 2
+if (process.argv.slice(2).length > 2) {
+  console.log('Invalid number of parameters entered.');
+  process.exit();
+}
+
+//seperate out FILEPATH and the URI
 const URI = process.argv.slice(2, 3).toString();
 const FILEPATH = process.argv.slice(3, 4).toString();
 
-console.log('URI:', URI);
-console.log('FILEPATH:', FILEPATH);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 
 
 request(URI, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-
-  if (!error) {
-    fs.writeFile(`./${FILEPATH}`, `${body}`, (err) => {
-      if (!err) {
-        console.log(`Downloaded and saved 1235 bytes to ${FILEPATH}. `);
-    
-      } else {
-        console.log('error:', err);
+  //start writing file with checkIfExists as the callback
+  const checkIfExists = function(err) {
+    //no error if file doesn't exist
+    if (!err) {
+      //just confirm it is written
+      confirmation();
+      //make sure to close rl so we don't hang if not overwriting
+      rl.close();
+    } else {
+      //file exists...
+      console.log('File exists...');
+      
+      //handle already exists error code
+      if (err.code === 'EEXIST') {
+        //handle overwrite confirmation input
+        rl.question('File already exists. Do you want to overwrite ([y]es/[n]o):', (answer) => {
+          if (answer === 'yes' || answer === 'y') {
+            //if yes write file.
+            fs.writeFile(`./${FILEPATH}`, `${body}`, confirmation);
+          } else if (answer === 'no' || answer === 'n') {
+            console.log(`File ${FILEPATH} not overwritten.`);
+          } else {
+            console.log('Invalid input.');
+          }
+          rl.close();
+        });
+      } else
         throw err;
-      }
-    });
+    }
+  };
+
+  //first step:
+  //if no error in request, start writing file.
+  if (!error) {
+    console.log('Downloading...');
+    fs.writeFile(`./${FILEPATH}`, `${body}`, {flag: 'wx'}, checkIfExists);
+  } else {
+    // Print the error if one occurred
+    console.log('error:', error);
   }
-
-
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
 });
+
+const confirmation = function() {
+  let STATS = fs.statSync(FILEPATH);
+  console.log(`Downloaded and saved ${STATS.size} bytes to ./${FILEPATH}. `);
+};
